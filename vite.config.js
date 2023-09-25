c960928e0189e391138a43f423080fc4353c8c45
Jonny Thaw <hi@jthaw.me>
2023-09-25 16:17:14 +0100

    fix: added some shit for manifest

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,38 @@ import svg from '@poppanator/sveltekit-svg'
 import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
-  plugins: [sveltekit(), SvelteKitPWA(), svg(), Icons({
+  plugins: [sveltekit(), SvelteKitPWA({
+    manifest: {
+      name: "Life Counter",
+      short_name: "Life",
+      description: 'Minimal life counter for MTG games',
+      theme_color: '#111',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ]
+    }
+  }), svg(), Icons({
     compiler: 'svelte',
   })],
 
